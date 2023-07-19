f61beac5f489f71d2f7431377ced843ed3f60a35
Jessica Sampaio <jesssampaio@icloud.com>
2023-07-19 10:13:40 -0400

    create post route

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 from flask import Flask
 from routes.users import *
+from routes.posts import *
 
 app = Flask(__name__)
 
 app.register_blueprint(users)
+app.register_blueprint(posts)
 
 
 @app.route('/')
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "Hello, World!"
 
 
 if __name__ == "__main__":
