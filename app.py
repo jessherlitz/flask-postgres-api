f4d120116a9f3c0500b27704e0677e5e32c4f34b
Jessica Sampaio <jesssampaio@icloud.com>
2023-07-19 11:19:29 -0400

    create like route

--- app.py
+++ app.py
@@ -2,12 +2,14 @@ from flask import Flask
 from routes.users import *
 from routes.posts import *
 from routes.follows import *
+from routes.likes import *
 
 app = Flask(__name__)
 
 app.register_blueprint(users)
 app.register_blueprint(posts)
 app.register_blueprint(follows)
+app.register_blueprint(likes)
 
 
 @app.route('/')
